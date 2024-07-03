635f41371a996a209c24835f754ca9f35af91c5f
normaliner <prokopenko.viktor110701@mail.ru>
2024-07-03 16:38:13 +0300

    Project settings: fonts, loading, providers, SEO

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import Loader from '@/components/ui/Loader';
+
+const Loading = () => {
+	return (
+		<div className='h-[100vh] w-full flex items-center justify-center'>
+			<Loader />
+		</div>
+	);
+};
+
+export default Loading;
