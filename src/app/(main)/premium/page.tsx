52199aeb33ef0879097d7240708246401ea391b4
normaliner <prokopenko.viktor110701@mail.ru>
2024-07-10 17:10:38 +0300

    Create payment-page

--- src/app/(main)/premium/page.tsx
+++ src/app/(main)/premium/page.tsx
@@ -1,9 +1,11 @@
 import { Metadata } from 'next';
 
+import Premium from './Premium';
+
 export const metadata: Metadata = {
 	title: 'Премиум подписка',
 };
 
 export default function PremiumPage() {
-	return <div>Premium</div>;
+	return <Premium />;
 }
---
+++ src/app/(main)/thanks/page.tsx
@@ -0,0 +1,30 @@
+import { Metadata } from 'next';
+import Link from 'next/link';
+
+import Button from '@/components/ui/form-elements/button/Button';
+import Heading from '@/components/ui/heading/Heading';
+
+import { DASHBOARD_URL } from '@/config/url.config';
+
+import { NO_INDEX_PAGE } from '@/constants/seo.constants';
+
+import styles from './Thanks.module.scss';
+
+export const metadata: Metadata = {
+	title: 'Успешная покупка',
+	...NO_INDEX_PAGE,
+};
+
+export default function ThanksPage() {
+	return (
+		<div className={styles.wrapper}>
+			<div className={styles.area}>
+				<Heading className={styles.heading}>Успешная покупка</Heading>
+				<p>Спасибо Вам за покупку подписки на нашем сайте</p>
+				<Link href={DASHBOARD_URL.root()}>
+					<Button>Перейти в личный кабинет</Button>
+				</Link>
+			</div>
+		</div>
+	);
+}
